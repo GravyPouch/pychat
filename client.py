24753f2b03308a7cd54bfc1dfbd8f4ac30da495d
GravyPouch <29843812+GravyPouch@users.noreply.github.com>
2018-12-11 00:06:31 -0500

    Add files via upload
    New version

--- client.py
+++ client.py
@@ -1,29 +1,51 @@
-import socket,sys,time,threading
+import socket,sys,time,threading,sys
 from threading import Thread
+#setting up some variables for later use
 x = 5
+#making calling the socket eaiser
 s = socket.socket()
+#entering server
 host = input(str("Please enter the ip/hostname of the server : "))
 port = 8080
+#tries to connect to provided ip and port
 try:
     s.connect((host,port))
 except:
+    #if it cant it will exit the program
     print("cannot connect to server try again.")
-
+    sys.exit()
+#defines how the program interprets recived packets
 def recive():
+    #loop
     while 1:
+        #Defines the packet size and what to call it
         incoming_message = s.recv(1024)
+        #decodes the message from byte like object to str
         incoming_message = incoming_message.decode()
-        print(" Server : ", incoming_message)
-        print("")
-
+        #if the message is something it will print it.
+        if incoming_message != '':
+            print("Server: ", incoming_message)
+        else:
+            continue
+#a function to start a reciving thread
+def startfrecive():
+    frecive = threading.Thread(target=recive)
+    frecive.start()
+#a function to send text to server
 def send():
     while 1:
-        message = input(": ")
+        #gets input
+        message = input("// ")
+        #encodes it for transport
         message = message.encode('ascii')
+        #sends it
         s.send(message)
+#a function to start a thread to send things
+def startfsend():
+    fsend = threading.Thread(target=send)
+    fsend.start()
 
 print("Connected to chat server")
-threading.Thread(target=recive())
-
-while 1:
-    pass
+#starts threads
+startfrecive()
+startfsend()
--- server.py
+++ server.py
@@ -1,41 +1,63 @@
-import socket,sys,time,_thread,threading
+import socket,sys,time,threading,random
 from threading import Thread
-
-exitflag = 0
+#defines vars
 x = 1
+#makes socket eaiser to type
 s = socket.socket()
+#define server ip or hostname
 host = ''
 print(" server will start on host : ", host)
 port = 8080
+#binds the program network interface to the ip and port
 s.bind((host,port))
 
-def timelapse(x):
+#defines a recive function
+def recive(conn,addr):
+#loop
     while 1:
-        time.sleep(1)
-        x = x + 1
-        print(x)
+#tries to recive pakcets and difine them if the client dissconnects the server searchs for a new client
+        try:
+            incoming_message = conn.recv(1024)
+        except ConnectionResetError:
+            print("error client lost")
+            print("Dont worry we find a new one")
+            print("Spinning up a new thread")
+            startfconnections()
+            #sets a varabile to decode any messsages
+        incoming_message = incoming_message.decode()
+        #if the message isnt blank
+        if incoming_message != '':
+            #this prints the messages then sends it back to client with a tad delay
+            print(" Client : ", incoming_message)
+            time.sleep(0.1)
+            #re encodes the message
+            incoming_message = incoming_message.encode('ascii')
+            #sends it
+            conn.send(incoming_message)
+        else:
+            continue
 
-def recive(x,conn,addr):
-    incoming_message = conn.recv(1024)
-    incoming_message = incoming_message.decode()
-    print(" Client : ", incoming_message)
-    print("")
+def startfrecive(conn,addr):
+    #starts a reciving thread to recivce messages
+    ftimer = threading.Thread(target=recive(conn,addr))
+    ftimer.start()
 
-def connections(x):
+def connections():
     print("Waiting for connections...")
+    #listens for clients then connects them
     s.listen(1)
     conn, addr = s.accept()
+    #sends a confirmation message to client
     print(addr, " has joined")
     message = str("Welcome")
     message = message.encode('ascii')
     conn.send(message)
+    #starts a thread to start reciving messages
+    startfrecive(conn,addr)
 
-
-connectionmaker = threading.Thread(target=connections(x))
-print("hwllo")
-connectionmaker.start()
-
-while 1:
-   pass
-
-  #https://www.tutorialkart.com/python/python-multithreading/#example
+def startfconnections():
+    # a function to start connection threads
+    ftimer = threading.Thread(target=connections)
+    ftimer.start()
+#starts function
+startfconnections()
